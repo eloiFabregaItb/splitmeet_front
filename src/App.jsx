6d1a66e13de9ea6c525b9aa806d91de3f1d9a351
DavidCardenas0403 <david.cardenas.7e6@itb.cat>
2023-10-16 16:55:50 +0200

    First connection with socket.io with the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import { Server } from "socket.io";
+import { useEffect, useState } from "react";
+import { io, Manager } from "socket.io-client";
+const url = "http://172.30.4.55:3000";
 
 function App() {
+    const [ioSocket, setIoSocket] = useState(null);
+
+    useEffect(() => {
+        const socket = io(url);
+        setIoSocket(socket);
+        socket.on("chatMsg", (msg) => {
+            console.log(msg);
+        });
+
+        return () => {
+            if (socket) {
+                socket.disconnect();
+            }
+        };
+    }, []);
+
     return (
         <>
             <h1>Splimeet</h1>
+            <button
+                onClick={() => {
+                    ioSocket.emit("chatMsg", "hola");
+                }}
+            >
+                Hola
+            </button>
         </>
     );
 }
