93e2217dc340646a84247729c1cdd74933721547
DavidCardenas0403 <david.cardenas.7e6@itb.cat>
2023-10-20 16:07:24 +0200

    axios installed + login connected with backend

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,8 @@ import facebook from "../../assets/icons/facebook.svg";
 import candado from "../../assets/icons/password.svg";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { SHA256 } from "crypto-js";
+import { checkLogin } from "../../services/checkLogin";
 
 function Login() {
     const [email, setEmail] = useState("");
@@ -37,105 +39,106 @@ function Login() {
     /*
     Function that checks the email structure and sends the email and password to the server.
     */
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        //console.log(SHA256(password).toString());
         //If the email has the proper structure, then the email and password is sent to the server.
         if (!checkEmailError()) {
-            checkData();
+            checkLogin(email, password);
         }
     };
 
     return (
         <>
-            <div className='contenedor'>
+            <div className="contenedor">
                 <main
                     className={`login ${
                         showEmailError || showDataError ? "login--error" : ""
                     }`}
                 >
                     <img
-                        className='login_logo'
+                        className="login_logo"
                         src={logo}
-                        alt='Logo de Splitmeet'
+                        alt="Logo de Splitmeet"
                     />
-                    <h1 className='login_titulo'>SplitMeet</h1>
+                    <h1 className="login_titulo">SplitMeet</h1>
 
-                    <form noValidate onSubmit={onSubmit} className='login_form'>
-                        <div className='login_form_inputContainer'>
+                    <form noValidate onSubmit={onSubmit} className="login_form">
+                        <div className="login_form_inputContainer">
                             <img
                                 src={user}
-                                alt='Logo de usuario'
-                                className='login_form_logo'
+                                alt="Logo de usuario"
+                                className="login_form_logo"
                             />
                             <input
-                                className='login_form_input'
-                                type='email'
-                                placeholder='Email'
-                                name='email'
-                                id='email'
+                                className="login_form_input"
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                                id="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
 
-                        <div className='login_form_inputContainer'>
+                        <div className="login_form_inputContainer">
                             <img
                                 src={candado}
-                                alt='Logo de un candado/password'
-                                className='login_form_logo'
+                                alt="Logo de un candado/password"
+                                className="login_form_logo"
                             />
                             <input
-                                className='login_form_input'
-                                type='password'
-                                placeholder='Contraseña'
-                                name='password'
-                                id='password'
+                                className="login_form_input"
+                                type="password"
+                                placeholder="Contraseña"
+                                name="password"
+                                id="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
 
                         {showEmailError ? (
-                            <span className='error'>
+                            <span className="error">
                                 Email con formato incorrecto
                             </span>
                         ) : (
                             ""
                         )}
 
-                        <button className='login_form_btn login_form_btn--login'>
+                        <button className="login_form_btn login_form_btn--login">
                             LOGIN
                         </button>
                     </form>
 
-                    <p className='login_mensajeRegistro'>
+                    <p className="login_mensajeRegistro">
                         ¿No tienes una cuenta?
-                        <Link to='/signup' className='subrayado'>
+                        <Link to="/signup" className="subrayado">
                             Regístrate
                         </Link>
                     </p>
 
-                    <div className='login_buttons'>
+                    <div className="login_buttons">
                         <a
-                            href='#'
-                            className='login_form_btn login_form_btn--google'
+                            href="#"
+                            className="login_form_btn login_form_btn--google"
                         >
                             <img
-                                className='login_buttons_logo'
+                                className="login_buttons_logo"
                                 src={google}
-                                alt='Logo de Google'
+                                alt="Logo de Google"
                             />
                             Continue with Google
                         </a>
 
                         <a
-                            href='#'
-                            className='login_form_btn login_form_btn--facebook'
+                            href="#"
+                            className="login_form_btn login_form_btn--facebook"
                         >
                             <img
-                                className='login_buttons_logo'
+                                className="login_buttons_logo"
                                 src={facebook}
-                                alt='Logo de Facebook'
+                                alt="Logo de Facebook"
                             />
                             Continue with Facebook
                         </a>
