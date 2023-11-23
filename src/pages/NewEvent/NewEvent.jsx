c2e1c99b2387a14e6d848d3be07eeed7c4aa7ec7
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2023-11-23 15:22:10 +0100

    home y navbar responsive

--- src/pages/NewEvent/NewEvent.jsx
+++ src/pages/NewEvent/NewEvent.jsx
@@ -41,74 +41,70 @@ function NewEvent() {
   };
   return (
     <>
-      <div className='container'>
-        <main className='box'>
+      <div className="container">
+        <main className="box">
           <p>{showDataError}</p>
-          <h1 className='newevent__title'>New event</h1>
+          <h1 className="newevent__title">New event</h1>
 
-          <form noValidate onSubmit={onSubmit} className='event_form'>
-            <div className='form-container'>
-
+          <form noValidate onSubmit={onSubmit} className="event_form">
+            <div className="form-container">
               <div className="add_name">
-              <label className='newevent__text' htmlFor='name'>
-                Event Name
-              </label>
-              <div className='newevent__form_inputContainer'>
-                <input
-                  className='newevent__form_input'
-                  type='text'
-                  placeholder='event Name'
-                  name='event_name'
-                  id='event_name'
-                  value={event_name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
+                <label className="newevent__text" htmlFor="name">
+                  Event Name
+                </label>
+                <div className="newevent__form_inputContainer">
+                  <input
+                    className="newevent__form_input"
+                    type="text"
+                    placeholder="event Name"
+                    name="event_name"
+                    id="event_name"
+                    value={event_name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
               </div>
 
-
-              <div className='add_image'>
-                <label className='newevent__text' htmlFor='event_image'>
+              <div className="add_image">
+                <label className="newevent__text" htmlFor="event_image">
                   Event Image
                 </label>
 
                 <input
-                  type='file'
+                  type="file"
                   ref={inputFileRef}
-                  className='newevent__inputFile'
-                  name='event_image'
-                  id='event_image'
+                  className="newevent__inputFile"
+                  name="event_image"
+                  id="event_image"
                   value={event_image}
                   onChange={(e) => setImage(e.target.value)}
                 />
                 <button
-                  type='button'
-                  className='newevent__buttonFile'
+                  type="button"
+                  className="newevent__buttonFile"
                   onClick={handleInputFile}
                 >
                   Upload Image
                 </button>
               </div>
-              <div className='newevent__members'>
-                <label className='newevent__text' htmlFor='event_members'>
+              <div className="newevent__members">
+                <label className="newevent__text" htmlFor="event_members">
                   Event members
                 </label>
 
-                <div className='newevent__form_textarea'>
-                  <textarea
-                    className=''
-                    name='event_members'
-                    cols='33'
-                    rows='10'
-                    value={event_members}
-                    onChange={(e) => setMembers(e.target.value)}
-                  ></textarea>
-                </div>
+                <textarea
+                  className="newevent__form_textarea"
+                  name="event_members"
+                  cols="33"
+                  rows="10"
+                  value={event_members}
+                  onChange={(e) => setMembers(e.target.value)}
+                ></textarea>
               </div>
 
               <Button
-                className='newevent_form_btn newevent_form_btn--login'
-                text='SUBMIT'
+                className="newevent_form_btn newevent_form_btn--login"
+                text="SUBMIT"
               />
             </div>
           </form>
