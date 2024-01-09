66f357e8a277353314da09b5c5ed743c43c393aa
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2024-01-09 20:04:28 +0100

    avanzar Invitation.jsx

--- src/pages/Invitation/Invitation.jsx
+++ src/pages/Invitation/Invitation.jsx
@@ -29,6 +29,19 @@ function Invitation() {
   const [showDataError, setShowDataError] = useState(false);
   const navigate = useNavigate();
 
+  const addInvitation = () => {
+    console.log("hola");
+    setUserInvitations([...userInvitations, userMail]);
+  };
+
+  const validUserMail = (email) => {
+    if (userInvitations.includes(email)) return false;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(email)) return false;
+    return true;
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -46,59 +59,64 @@ function Invitation() {
   return (
     <>
       <Header nameEvent={eventInfo.event.name}></Header>
-      <div className='container'>
-        <main className='box'>
+      <div className="container">
+        <main className="box">
           <p>{showDataError}</p>
-          <h1 className='newevent__title'>Invite Users</h1>
+          <h1 className="newevent__title">Invite Users</h1>
 
           <form
             noValidate
             onSubmit={onSubmit}
-            className='event_form event_form--invitation'
+            className="event_form event_form--invitation"
           >
-            <div className='form-container form-container--invitation'>
-              <div className='form-container__container-invitation'>
-                <div className='add_name'>
-                  <label className='newevent__text' htmlFor='userMail'>
+            <div className="form-container form-container--invitation">
+              <div className="form-container__container-invitation">
+                <div className="add_name">
+                  <label className="newevent__text" htmlFor="userMail">
                     User email
                   </label>
-                  <div className='newevent__form_inputContainer newevent__form_inputContainer--invitation'>
+                  <div className="newevent__form_inputContainer newevent__form_inputContainer--invitation">
                     <input
-                      className='newevent__form_input newevent__form_input--invitation'
-                      type='text'
-                      placeholder='user-example@gmail.com'
-                      name='userMail'
-                      id='userMail'
+                      className="newevent__form_input newevent__form_input--invitation"
+                      type="text"
+                      placeholder="user-example@gmail.com"
+                      name="userMail"
+                      id="userMail"
                       value={userMail}
                       onChange={(e) => setUserMail(e.target.value)}
                     />
-                    <button className='invitation__button invitation__plusButton'>
+                    <button
+                      className="invitation__button invitation__plusButton"
+                      onClick={addInvitation}
+                      /* disabled={() => !validUserMail(userMail)} */
+                      type="button"
+                    >
                       +
                     </button>
                   </div>
                 </div>
 
-                <div className='newevent__members'>
-                  <label className='newevent__text' htmlFor='event_members'>
+                <div className="newevent__members">
+                  <label className="newevent__text" htmlFor="event_members">
                     Invitations
                   </label>
-                  <div className='newevent__form_inputContainer newevent__form_inputContainer--invitation'>
+                  <div className="newevent__form_inputContainer newevent__form_inputContainer--invitation">
                     <input
-                      className='newevent__form_input newevent__form_input--invitation'
-                      type='text'
-                      placeholder='user-example@gmail.com'
+                      className="newevent__form_input newevent__form_input--invitation"
+                      type="text"
+                      placeholder="user-example@gmail.com"
                     />
-                    <button className='invitation__button invitation__crossButton'>
+                    <button className="invitation__button invitation__crossButton">
                       x
                     </button>
                   </div>
-                  <div className='newevent__form_inputContainer newevent__form_inputContainer--invitation'>
+                  <div className="newevent__form_inputContainer newevent__form_inputContainer--invitation">
                     <input
-                      className='newevent__form_input newevent__form_input--invitation'
-                      type='text'
-                      placeholder='user-example@gmail.com'
+                      className="newevent__form_input newevent__form_input--invitation"
+                      type="text"
+                      placeholder="user-example@gmail.com"
                     />
-                    <button className='invitation__button invitation__crossButton'>
+                    <button className="invitation__button invitation__crossButton">
                       x
                     </button>
                   </div>
@@ -106,8 +124,8 @@ function Invitation() {
               </div>
 
               <Button
-                className='newevent_form_btn newevent_form_btn--login'
-                text='INVITE'
+                className="newevent_form_btn newevent_form_btn--login"
+                text="INVITE"
               />
             </div>
           </form>
---
+++ src/pages/Invitation/UserInvitation/UserInvitation.jsx
@@ -0,0 +1,10 @@
+function UserInvitation() {
+  return (
+    <li>
+      <p></p>
+      <button></button>
+    </li>
+  );
+}
+
+export default UserInvitation;
