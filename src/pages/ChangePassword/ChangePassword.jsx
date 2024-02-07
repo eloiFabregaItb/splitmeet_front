6df236b5257f2150352f0df202af6bb8c026b09c
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2024-02-07 17:05:15 +0100

    cambiar iconos ojos y avanzar change password

--- src/pages/ChangePassword/ChangePassword.jsx
+++ src/pages/ChangePassword/ChangePassword.jsx
@@ -1,66 +1,66 @@
-import '../Login/Login.css'
-import '../ForgotPassword/ForgotPassword.css'
+import "../Login/Login.css";
+import "../ForgotPassword/ForgotPassword.css";
 
-import Header from '../../globalComponents/Header/Header'
-import Button from '../../globalComponents/Button'
-import logo from '../../assets/icons/logo.svg'
-import candadoAbierto from '../../assets/icons/candadoAbierto.svg'
-import candadoCerrado from '../../assets/icons/candadoCerrado.svg'
-import { changePassword } from '../../services/changePassword'
-import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import Header from "../../globalComponents/Header/Header";
+import Button from "../../globalComponents/Button";
+import logo from "../../assets/icons/logo.svg";
+import candadoAbierto from "../../assets/icons/candadoAbierto.svg";
+import candadoCerrado from "../../assets/icons/candadoCerrado.svg";
+import { changePassword } from "../../services/changePassword";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 function ChangePassword() {
-  const params = useParams()
-  const errMsg = 'An error occurred while sending the email'
-  const succMsg = 'The email has been sent successfully'
-  const passError = 'Passwords do not much'
-  const [password, setPassword] = useState('')
-  const [passwordConfirmation, setPasswordConfirmation] = useState('')
-  const [showPassword, setShowPassword] = useState(false)
+  const params = useParams();
+  const errMsg = "An error occurred while sending the email";
+  const succMsg = "The email has been sent successfully";
+  const passError = "Passwords do not much";
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [showPasswordConfirmation, setShowPasswordConfirmation] =
-    useState(false)
-  const [showError, setShowError] = useState(false)
-  const [showPassEqualError, setShowPassEqualError] = useState(false)
-  const [showSuccess, setShowSuccess] = useState(false)
+    useState(false);
+  const [showError, setShowError] = useState(false);
+  const [showPassEqualError, setShowPassEqualError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const checkPassEqualError = (pass1, pass2) => {
     if (pass1 !== pass2) {
-      setShowPassEqualError(true)
-      return true
+      setShowPassEqualError(true);
+      return true;
     }
-    setShowPassEqualError(false)
-    return false
-  }
+    setShowPassEqualError(false);
+    return false;
+  };
 
   const sendChangePasswordRequest = async (e) => {
-    e.preventDefault()
-    setShowError(false)
-    setShowSuccess(false)
-    setShowPassEqualError(false)
+    e.preventDefault();
+    setShowError(false);
+    setShowSuccess(false);
+    setShowPassEqualError(false);
 
     if (checkPassEqualError(password, passwordConfirmation)) {
-      setShowPassEqualError(true)
-      return
+      setShowPassEqualError(true);
+      return;
     }
 
-    const resChangePassword = await changePassword(params.jwt, password)
+    const resChangePassword = await changePassword(params.jwt, password);
     if (resChangePassword.success) {
-      setShowSuccess(true)
-      return
+      setShowSuccess(true);
+      return;
     }
 
-    setShowError(true)
-  }
+    setShowError(true);
+  };
 
   return (
     <>
       <Header></Header>
-      <div className='contenedor'>
-        <main className='login'>
-          <img className='login_logo' src={logo} alt='Logo de Splitmeet' />
-          <h1 className='login_titulo'>SplitMeet</h1>
-          <p className='verify_text'>Set your new password</p>
+      <div className="contenedor">
+        <main className="login">
+          <img className="login_logo" src={logo} alt="Logo de Splitmeet" />
+          <h1 className="login_titulo">SplitMeet</h1>
+          <p className="verify_text">Set your new password</p>
           {/*           <input
             type='password'
             className='input mb-10'
@@ -70,64 +70,64 @@ function ChangePassword() {
           /> */}
           <form
             noValidate
-            className='flex-center'
+            className="flex-center"
             onSubmit={(e) => sendChangePasswordRequest(e)}
           >
-            <div className='login_form_inputContainer mb-20'>
+            <div className="login_form_inputContainer mb-20">
               <img
                 src={showPassword ? candadoAbierto : candadoCerrado}
-                alt='Logo de un candado/password'
-                className='login_form_logo'
+                alt="Logo de un candado/password"
+                className="login_form_logo"
                 onClick={() => setShowPassword(!showPassword)}
               />
               <input
-                className='login_form_input'
-                type={showPassword ? 'text' : 'password'}
-                placeholder='Password'
-                name='password'
-                id='password'
+                className="login_form_input"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className='login_form_inputContainer mb-20'>
+            <div className="login_form_inputContainer mb-20">
               <img
                 src={showPasswordConfirmation ? candadoAbierto : candadoCerrado}
-                alt='Logo de un candado/password'
-                className='login_form_logo'
+                alt="Logo de un candado/password"
+                className="login_form_logo"
                 onClick={() =>
                   setShowPasswordConfirmation(!showPasswordConfirmation)
                 }
               />
               <input
-                className='login_form_input'
-                type={showPasswordConfirmation ? 'text' : 'password'}
-                placeholder='Password'
-                name='passwordConfirmation'
-                id='passwordConfirmation'
+                className="login_form_input"
+                type={showPasswordConfirmation ? "text" : "password"}
+                placeholder="Password"
+                name="passwordConfirmation"
+                id="passwordConfirmation"
                 value={passwordConfirmation}
                 onChange={(e) => setPasswordConfirmation(e.target.value)}
               />
             </div>
             <Button
-              classname='login_form_btn login_form_btn--login'
-              text='VERIFY'
+              classname="login_form_btn login_form_btn--login mt-30"
+              text="VERIFY"
             />
           </form>
           {(showError || showSuccess) && (
             <span
               className={
-                showError ? 'error pt-40' : showSuccess && 'success pt-40'
+                showError ? "error pt-40" : showSuccess && "success pt-40"
               }
             >
-              {showError && errMsg} {showSuccess && succMsg}{' '}
+              {showError && errMsg} {showSuccess && succMsg}{" "}
               {showPassEqualError && passError}
             </span>
           )}
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default ChangePassword
+export default ChangePassword;
--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,60 +1,60 @@
-import '../Login/Login.css'
-import './ForgotPassword.css'
+import "../Login/Login.css";
+import "./ForgotPassword.css";
 
-import Header from '../../globalComponents/Header/Header'
-import Button from '../../globalComponents/Button'
-import logo from '../../assets/icons/logo.svg'
-import { sendForgottenPasswordEmail } from '../../services/sendForgottenPasswordEmail'
-import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import Header from "../../globalComponents/Header/Header";
+import Button from "../../globalComponents/Button";
+import logo from "../../assets/icons/logo.svg";
+import { sendForgottenPasswordEmail } from "../../services/sendForgottenPasswordEmail";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 function ForgotPassword() {
-  const params = useParams()
-  const errMsg = 'An error occurred while sending the email'
-  const succMsg = 'The email has been sent successfully'
-  const [email, setEmail] = useState('')
-  const [showError, setShowError] = useState(false)
-  const [showSuccess, setShowSuccess] = useState(false)
+  const params = useParams();
+  const errMsg = "An error occurred while sending the email";
+  const succMsg = "The email has been sent successfully";
+  const [email, setEmail] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const sendEmail = async () => {
-    const resSendEmail = await sendForgottenPasswordEmail(params.jwt, email)
+    const resSendEmail = await sendForgottenPasswordEmail(params.jwt, email);
     if (resSendEmail.success) {
-      setShowError(false)
-      setShowSuccess(true)
-      return
+      setShowError(false);
+      setShowSuccess(true);
+      return;
     }
 
-    setShowSuccess(false)
-    setShowError(true)
-  }
+    setShowSuccess(false);
+    setShowError(true);
+  };
 
   return (
     <>
       <Header></Header>
-      <div className='contenedor'>
-        <main className='login'>
-          <img className='login_logo' src={logo} alt='Logo de Splitmeet' />
-          <h1 className='login_titulo'>SplitMeet</h1>
-          <p className='verify_text'>
+      <div className="contenedor">
+        <main className="login">
+          <img className="login_logo" src={logo} alt="Logo de Splitmeet" />
+          <h1 className="login_titulo">SplitMeet</h1>
+          <p className="verify_text">
             Write your email to receive a link to reset your password.
           </p>
           <input
-            type='text'
-            className='input mb-20'
+            type="text"
+            className="input mb-20"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
 
           <div onClick={sendEmail}>
             <Button
-              classname='login_form_btn login_form_btn--login'
-              text='VERIFY'
+              classname="login_form_btn login_form_btn--login"
+              text="VERIFY"
             />
           </div>
           {(showError || showSuccess) && (
             <span
               className={
-                showError ? 'error pt-40' : showSuccess && 'success pt-40'
+                showError ? "error pt-40" : showSuccess && "success pt-40"
               }
             >
               {showError ? errMsg : showSuccess && succMsg}
@@ -63,7 +63,7 @@ function ForgotPassword() {
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
