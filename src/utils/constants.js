a44914eab64fdddbf29e0352151111fb896e660c
orillad <orillad2003@gmail.com>
2024-02-08 15:29:28 +0100

    ORIOL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-//export const api_url = "http://172.30.4.55:3000";
-export const api_url = 'http://172.30.4.55:3000'
-//export const api_url = 'https://api.split-meet.com:4433'
+// export const api_url = "http://172.30.4.55:3000";
+// export const api_url = 'http://172.30.4.55:3000'
+export const api_url = 'http://localhost:3000'
+// export const api_url = 'https://api.split-meet.com:4433'
