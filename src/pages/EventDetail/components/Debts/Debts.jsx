70d08120f5d3295e140c52c1a5b0b5680ea3cc4a
orillad <orillad2003@gmail.com>
2024-02-15 16:44:36 +0100

    debts

---
+++ src/pages/EventDetail/components/Debts/Debts.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Debts.css"; // Asegúrate de importar correctamente el archivo de estilos
+import { api_url } from "../../../../utils/constants";
+
+function Debts({ creditor, debtor, amount, users }) {
+
+    return (
+        <article className='expense' >
+            <div className='expense_img-concept'>
+                <img
+                    src={`https://robohash.org/${creditor}`} 
+                    alt={`Icono del usuario ${creditor}`} 
+                />
+                <p>{creditor}</p>
+            </div>
+            <p>→</p>
+            <div className='expense_img-concept'>
+                <img
+                    src={`https://robohash.org/${debtor}`} 
+                    alt={`Icono del usuario ${debtor}`} 
+                />
+                <p>{debtor}</p>
+            </div>
+
+            <p className={`expense_amount ${amount > 0 ? "expense_amount--red" : "expense_amount--green"}`}>
+                {amount > 0 ? `You owe ${amount}€ to ${creditor}` : `You lent ${-amount}€ to ${debtor}`}
+            </p>
+        </article>
+    );
+}
+
+export default Debts;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const api_url = "http://172.30.4.55:3000";
-// export const api_url = 'http://172.30.4.55:3000'
-export const api_url = 'http://localhost:3000'
+export const api_url = 'http://172.30.4.55:3000'
+// export const api_url = 'http://localhost:3000'
 // export const api_url = 'https://api.split-meet.com:4433'
 
 
