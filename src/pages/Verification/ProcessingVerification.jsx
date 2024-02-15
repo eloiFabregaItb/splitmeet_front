87001c853a6cb1582d205e264873fa73845af6f1
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2024-02-15 15:43:16 +0100

    arreglar error al verificar email

--- src/pages/Verification/ProcessingVerification.jsx
+++ src/pages/Verification/ProcessingVerification.jsx
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react";
-import { validateMail } from "../../services/ValidateMail";
-import { checkLoginJwt } from "../../services/checkLoginJwt";
-import { useLoginDataContext } from "../../contexts/LoginDataContext";
-import Loader from "../../globalComponents/Loader/Loader";
-import Header from "../../globalComponents/Header/Header";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { validateMail } from '../../services/ValidateMail'
+import { checkLoginJwt } from '../../services/checkLoginJwt'
+import { useLoginDataContext } from '../../contexts/LoginDataContext'
+import Loader from '../../globalComponents/Loader/Loader'
+import Header from '../../globalComponents/Header/Header'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 
 function ProcessingVerification() {
-  const { loginContext } = useLoginDataContext();
-  const params = useSearchParams();
-  const navigate = useNavigate();
-  const jwt = params[0].get("id");
+  const { loginContext } = useLoginDataContext()
+  const params = useSearchParams()
+  const navigate = useNavigate()
+  const jwt = params[0].get('id')
 
   useEffect(() => {
     const validateEmail = async () => {
-      const resValidateMail = await validateMail(jwt);
+      const resValidateMail = await validateMail(jwt)
       if (resValidateMail.success) {
         //ARREGLAR ERROR TOKEN (NO SON IGUALES)
-        const resCheckLogin = await checkLoginJwt(jwt);
+        /*         const resCheckLogin = await checkLoginJwt(jwt);
         if (resCheckLogin.success) {
           loginContext(resCheckLogin);
           localStorage.setItem("jwt", resCheckLogin.jwt);
           return navigate("/home");
-        }
-        return navigate("/login");
+        } */
+        return navigate('/login')
       }
-      return navigate(`/verification?jwt=${jwt}`);
-    };
+      return navigate(`/verification?jwt=${jwt}`)
+    }
 
-    validateEmail();
-  }, []);
+    validateEmail()
+  }, [])
 
   return (
     <>
       <Header></Header>
       <Loader></Loader>
     </>
-  );
+  )
 }
 
-export default ProcessingVerification;
+export default ProcessingVerification
