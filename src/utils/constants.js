666ef05c73f44af30cf8a996753b48fae2b255f9
orillad <orillad2003@gmail.com>
2024-02-16 16:23:29 +0100

    constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // export const api_url = "http://172.30.4.55:3000";
 // export const api_url = 'http://172.30.4.55:3000'
-export const api_url = 'http://localhost:3000'
-// export const api_url = 'https://api.split-meet.com:4433'
+// export const api_url = 'http://localhost:3000'
+export const api_url = 'https://api.split-meet.com:4433'
 
 
 export const UNSPLASH_API_KEY = "JE5Gz8bXBxd5DDlov7sYXUAKZUiSaqZyIKE9od0jPlg"
