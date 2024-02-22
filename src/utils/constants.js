db168120932f0261f732c1865002c935cfedb00a
encarbassot <elgrefa@gmail.com>
2024-02-22 23:03:17 +0100

    minor

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const api_url = "http://172.30.1.15:3000";
+export const api_url = "http://localhost:3000";
 // export const api_url = 'http://localhost:3000'
 // export const api_url = 'https://api.split-meet.com:4433'
 
